03beefb567e054f6da409c817a4ff889267ecc10
pdek1991 <pdek1991@gmail.com>
2023-04-02 10:58:59 +0530

    scp

--- main.sh
+++ main.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -o User=root -o Password=root /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
+#scp -o StrictHostKeyChecking=no -o User=root -o Password=root /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
+
+#sshpass -p 'root' ssh root@192.168.56.11 
+
+
+
+sshpass -p 'root' scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
+
+
+#scp /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
 
 #scp /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
 
