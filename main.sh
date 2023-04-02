e77a2cb1409dd2116d81a4c989113e98ea541e4a
pdek1991 <pdek1991@gmail.com>
2023-04-02 11:11:23 +0530

    scp4

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@
 
 sshpass -p 'root' scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
 
-sshpass -p 'root' ssh ^root@192.168.56.11 'ansible-playbook /root/playbooks/pb1.yml'
+sshpass -p 'root' ssh root@192.168.56.11 'ansible-playbook /root/playbooks/pb1.yml'
 #scp /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
 
 #scp /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
