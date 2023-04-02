0c10f09b447a4ee21eb44dc2c13182d1fc8ffc3e
pdek1991 <pdek1991@gmail.com>
2023-04-02 10:49:43 +0530

    second script added..

--- main.sh
+++ main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-scp /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
+scp -o StrictHostKeyChecking=no -o User=root -o Password=root /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
+
+#scp /var/lib/jenkins/workspace/Copy-scripts/file.sh root@192.168.56.11:/tmp/file.sh
 
